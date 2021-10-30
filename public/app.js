638092bdf5a22d1f3aa0f2e5b38baa26c1139159
sarahecarter <secarter213@gmail.com>
2021-10-30 13:12:28 -0400

    starting dropdown filter content added

--- public/app.js
+++ public/app.js
@@ -1,6 +1,11 @@
+// Variables
 const $modal = $('.modal')
 const $close = $('.close')
+const $patterns = $('.pattern')
+const $filter = $('.filter')
+const $dropdown = $('.dropdown-content')
 
+// Modal Functionality
 $('.open-modal').click((e) => {
     $(e.target).parent().next().toggleClass('hide');
 })
@@ -8,3 +13,29 @@ $('.open-modal').click((e) => {
 $('.close').click((e) => {
     $(e.target).closest('.overlay').toggleClass('hide');
 })
+
+// Filter Functionality
+$($filter).click(() => {
+    $dropdown.toggleClass('hide');
+})
+
+// Filters
+const $chunky = $('.chunky')
+const $sweater = $('.sweater')
+const $beginner = $('.beginner')
+
+// Links
+const $chunkyA = $('#chunky-a')
+const $sweaterA = $('#sweater-a')
+const $beginnerA = $('#beg-a')
+
+// $beginnerA.click(() => {
+//     for (pattern of $patterns) {
+//         if (pattern.hasClass('beginner')) {
+//             pattern.toggleClass('hide');
+//         }
+//         else {
+//             pattern.toggleClass('hide');
+//         }
+//     }
+// })
